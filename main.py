696967d5de56ee6651cfbc0ccc79bb909644573a
Harry-CODE-HE <harryhaywardevans@gmail.com>
2021-06-19 09:33:17 +0100

    made database simpler

--- main.py
+++ main.py
@@ -5,22 +5,15 @@ import sqlite3
 from sqlite3 import Error
 from sqlite3.dbapi2 import version
 
+import sqlite3
 
-def create_connection(db_file):
-    """ create a database connection to a SQLite database """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
+con = sqlite3.connect('games.db')
+cur = con.cursor()
 
+# Create table
+cur.execute('''CREATE TABLE Users
+               (name text, rank int)''')
 
-if __name__ == '__main__':
-    create_connection(r"c:\users\public\sqlite")
 
 from typing import Optional
 
